65316f836639499a784d8ece72c942c76bf708ba
bramprasetyop <bramprasetyop@gmail.com>
2020-07-30 22:57:17 +0700

    fix profile web

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -37,23 +37,13 @@ class Body extends Component {
         <h1>Brama Prasetyo Permana</h1>
         <h3 style={{ color: 'red' }}>Software Engineer</h3>
         <h4 style={{ textAlign: 'justify' }}>
-          Hello World! My name is Brama Prasetyo Permana, you can call me Brama
-          or just Bram. I'm a Software Engineer living in Jakarta, Indonesia. I
-          primarily use Node.js for writing applications. I consider programming
-          as one of my passion . I am an open-minded individual with a proven
-          track record in designing websites and creating databases. I have
-          strong technical skills as well as excellent interpersonal skills. I
-          am eager to be challenged in order to grow and improve my
-          communication and professional IT skills gained through previous
-          experiences in the IT sector. Feel free to contact me if you wanted to
-          discuss something or just wanted to know more about me. You could
-          reach me using links below.
+          Hello World! My name is Brama Prasetyo Permana, you can call me Brama or just Bram. I'm a Software Engineer living in Jakarta, Indonesia. I primarily use Node.js for writing applications. I consider programming as one of my passion. I am an open-minded individual with a proven track record in designing websites and creating databases. I have strong technical skills as well as excellent interpersonal skills. I am eager to be challenged in order to grow and improve my communication and professional IT skills gained through previous experiences in the IT sector. Feel free to contact me if you wanted to discuss something or just wanted to know more about me. You could reach me using links below.
           <hr />
         </h4>
-        <Icon style={{cursor:'pointer'}} name="linkedin alternate" size="large" onClick={this.linkedin.bind(this)} />
-        <Icon style={{cursor:'pointer'}} name="github" size="large" onClick={this.github.bind(this)} />
-        <Icon style={{cursor:'pointer'}} name="envelope" size="large" onClick={this.emailPage.bind(this)} />
-        <Icon style={{cursor:'pointer'}} name="whatsapp" size="large" onClick={this.whatsapp.bind(this)} />
+        <Icon name="linkedin alternate" size="large" onClick={this.linkedin.bind(this)} />
+        <Icon name="github" size="large" onClick={this.github.bind(this)} />
+        <Icon name="envelope" size="large" onClick={this.emailPage.bind(this)} />
+        <Icon name="whatsapp" size="large" onClick={this.whatsapp.bind(this)} />
       </div>
     )
   }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,33 +1,3 @@
-// import React from 'react'
-// import Paper from '@material-ui/core/Paper';
-// import Grid from '@material-ui/core/Grid';
-
-
-// const GridExampleColumnCount = () => (
-
-//   // <Responsive>
-//   //   <Grid>
-//   //     <Grid.Row columns={7}>
-//   //       <Grid.Column />
-//   //       <Grid.Column />
-//   //       <Grid.Column />
-//   //       <Grid.Column>
-//   //         <Card className="Photo-image-card">
-//   //           <Image
-//   //             className="Photo-image"
-//   //             src="https://s3-ap-southeast-1.amazonaws.com/photo.all/Screenshot_2018-08-22-11-12-2_com.instagram.android_1524997588641.jpg"
-//   //           />
-//   //         </Card>
-//   //       </Grid.Column>
-//   //       <Grid.Column />
-//   //       <Grid.Column />
-//   //     </Grid.Row>
-//   //   </Grid>
-//   // </Responsive>
-// )
-
-// export default GridExampleColumnCount
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
